a949b529641585a91a9606b401fe1aa569d80c9d
clmateus <cl.mateuscosta@gmail.com>
2025-04-17 14:37:49 -0300

    aula

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCss = require("gulp-clean-css");
+const sourceMaps = require("gulp-sourcemaps");
+
+function compilaSass() {
+    // return gulp.src('./source/styles/*.scss')
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourceMaps.init())
+        .pipe(sass())
+        .pipe(sourceMaps.write('./maps'))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('./build/styles'));
+}
+
 function funcaoPadrao(callback) {
-    console.log("Executando via Gulp!");
-    callback();
+    setTimeout(function() {
+        console.log("Executando via Gulp!");
+        callback();
+    }, 2000) 
 }
 
 function dizOi(callback) {
@@ -13,5 +30,10 @@ function dizTchau() {
     console.log("Tchau!");
 }
 
-exports.default = funcaoPadrao;
+// exports.default = gulp.series(funcaoPadrao, dizOi);
+exports.default = gulp.parallel(funcaoPadrao, dizOi);
 exports.dizOi = dizOi;
+exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+}
